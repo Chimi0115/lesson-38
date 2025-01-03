98e126a0a0f09b3923f61087636eb08a0d935142
Chimi Yeshi Wangchuk <chimiyeshi2011@gmail.com>
2025-01-03 18:17:41 +0600

    L38A2

---
+++ activity2.py
@@ -0,0 +1,22 @@
+a=15
+b=7
+
+sum=a+b
+diff=a-b
+prod=a*b
+div=a/b
+floordiv=a//b
+mod=a%b
+
+print("Sum of ",a,"and ",b," is",sum)
+print("Differnce of ",a,"and ",b," is",diff)
+print("Product of ",a,"and ",b," is",prod)
+print("Divisiom of ",a,"and ",b," is",div)
+print("Floordiv of ",a,"and ",b," is",floordiv)
+print("Modulas of ",a,"and ",b," is",mod)
+print("2 to the power of 4 is",2**4)
+
+print("Is a equal to b",a==b)
+print("Is a not equal to b",a!=b)
+print("Is a greater than b",a>b)
+print("Is a lesser than b",a<b)
