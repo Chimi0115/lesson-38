c6eccd74fff8c201651b7c7709bb20171eaf6365
Chimi Yeshi Wangchuk <chimiyeshi2011@gmail.com>
2025-01-03 18:28:30 +0600

    L38A3

---
+++ activity3.py
@@ -0,0 +1,11 @@
+first_name="Chimi"
+middle_name="Yeshi"
+last_name="Wangchuk"
+full_name=first_name+middle_name+last_name
+
+name=first_name*5
+
+print("full name is",full_name)
+print("length of full name is", len(full_name))
+print(first_name[4])
+print(full_name[0:9])
