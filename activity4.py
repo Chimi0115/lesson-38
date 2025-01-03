428da0f8b0efe841abe2f26fff15e0e073ee7964
Chimi Yeshi Wangchuk <chimiyeshi2011@gmail.com>
2025-01-03 18:37:09 +0600

    L38A4

---
+++ activity4.py
@@ -0,0 +1,14 @@
+x=int(input("Enter your first number"))
+y=int(input("Enter your second number"))
+
+temp=0
+
+print("first number before swapping ",x)
+print("second number before swappinb ",y)
+
+temp=x
+x=y
+y=temp
+
+print("first number after swapping ",x)
+print("second number after swappinbg ",y)
